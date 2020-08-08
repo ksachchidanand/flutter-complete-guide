59c8ab67b309f9e4da36d1077ed33b57a62d01ee
rename-z <rename-z@pm.me>
2020-08-08 11:48:12 +0530

    app has started to get shape

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MyApp(),
-    );
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          leading: FlatButton(
-            onPressed: () {
-              print('hello');
-            },
-            child: Icon(
-              Icons.menu,
-              color: Colors.white,
-            ),
+          title: Text(
+            'My First App',
           ),
-          title: Text('MENU'),
         ),
-        body: Container(),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print('hello');
-          },
-          child: Icon(
-            Icons.add,
-          ),
+        body: Text(
+          'This is my default text',
         ),
       ),
     );
