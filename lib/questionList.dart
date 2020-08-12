88e688fcecdd9e11244429e2f516eb20e22fde25
rename-z <rename-z@pm.me>
2020-08-12 15:39:21 +0530

    restructured questionList and add score

--- lib/questionList.dart
+++ lib/questionList.dart
@@ -3,56 +3,131 @@ class QuestionList {
     {
       'question': 'It’s Saturday night, what are you up to?',
       'answers': [
-        'Creating something',
-        'Hosting 3-course curated dinners',
-        'Making a warm cup of tea',
-        'Catching up with friends or volunteering',
-        'Going on a date',
+        {
+          'text': 'Creating something',
+          'score': 1,
+        },
+        {
+          'text': 'Hosting 3-course curated dinners',
+          'score': 1,
+        },
+        {
+          'text': 'Making a warm cup of tea',
+          'score': 1,
+        },
+        {
+          'text': 'Catching up with friends or volunteering',
+          'score': 1,
+        },
+        {
+          'text': 'Going on a date',
+          'score': 2,
+        },
       ],
     },
     {
       'question': 'When do you have the best energy?',
       'answers': [
-        '6am-12pm',
-        '12pm-6pm',
-        '6pm-Midnight',
-        'Midnight-6am',
+        {
+          'text': '6am-12pm',
+          'score': 1,
+        },
+        {
+          'text': '12pm-6pm',
+          'score': 1,
+        },
+        {
+          'text': '6pm-Midnight',
+          'score': 1,
+        },
+        {
+          'text': 'Midnight-6am',
+          'score': 1,
+        },
       ],
     },
     {
       'question': 'What are you watching on Netflix?',
       'answers': [
-        'Gilmore Girls',
-        'Parks & Rec',
-        'Always Be My Maybe',
-        'Tales by Light',
-        'Queer Eye',
+        {
+          'text': 'Gilmore Girls',
+          'score': 1,
+        },
+        {
+          'text': 'Parks & Rec',
+          'score': 1,
+        },
+        {
+          'text': 'Always Be My Maybe',
+          'score': 1,
+        },
+        {
+          'text': 'Tales by Light',
+          'score': 1,
+        },
+        {
+          'text': 'Queer Eye',
+          'score': 1,
+        },
       ],
     },
     {
       'question': 'What’s your theme song?',
       'answers': [
-        'Daydream Believer – The Monkees',
-        'Run the World – Beyonce ',
-        'Photograph – Ed Sheeran',
-        'Can’t Knock the Hustle – Jay Z',
+        {
+          'text': 'Daydream Believer – The Monkees',
+          'score': 1,
+        },
+        {
+          'text': 'Run the World – Beyonce ',
+          'score': 1,
+        },
+        {
+          'text': 'Photograph – Ed Sheeran',
+          'score': 1,
+        },
+        {
+          'text': 'Can’t Knock the Hustle – Jay Z',
+        },
       ],
     },
     {
       'question': 'Which movie title best describes your life?',
       'answers': [
-        'Analyze This',
-        'Groundhog Day',
-        'Field of Dreams',
-        'American Hustle',
+        {
+          'text': 'Analyze This',
+          'score': 1,
+        },
+        {
+          'text': 'Groundhog Day',
+          'score': 1,
+        },
+        {
+          'text': 'Field of Dreams',
+          'score': 1,
+        },
+        {
+          'text': 'American Hustle',
+          'score': 1,
+        },
       ],
     },
     {
       'question': 'What’s your decision-making style?',
       'answers': [
-        'I often put off making decisions',
-        'I spend a lot of time writing pros and cons before making a decision',
-        'I trust myself to make decisions quickly and easily',
+        {
+          'text': 'I often put off making decisions',
+          'score': 1,
+        },
+        {
+          'text':
+              'I spend a lot of time writing pros and cons before making a decision',
+          'score': 1,
+        },
+        {
+          'text': 'I trust myself to make decisions quickly and easily',
+          'score': 1,
+        },
       ],
     },
   ];
